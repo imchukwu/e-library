5d82c30ae88960e8125e8068c6138226cefd1ef1
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-06-29 05:02:39 +0100

    Added the flow for complete user registration

--- src/main/java/com/cimspace/e_library/repos/UserRepository.java
+++ src/main/java/com/cimspace/e_library/repos/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
    Optional<Boolean> findByEmail(String email);
+
+   Optional<User> findUserByEmail(String email);
 }
